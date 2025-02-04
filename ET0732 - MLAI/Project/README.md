1284612b743f9f11c078d57611396fe9e9b7d043
BoonSiong <pohboonsiong540@gmail.com>
2025-02-04 14:15:36 +0800

    updated files

--- ET0732 - MLAI/Project/README.md
+++
@@ -1,1 +0,0 @@
-training_history.pkl is used for jupter notebook training data to show the graph
--- ET0736 - OOP&DS/Practical/Lab 8/Activity 8/src/Activity8_3.java
+++ ET0736 - OOP&DS/Practical/Lab 8/Activity 8/src/Activity8_3.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+
 
 public class Activity8_3 {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> aList = new MyLinkedList<>();
+        for (int x = 1; x <= 5; x++) {
+            aList.append(x);
+        }
+        System.out.println(aList);
+    }
+}
+
+class MyLinkedList<E> {
+    Node<E> head = null;
+    Node<E> current = null;
+    Node<E> newNode;
+    Node<E> tail = null;
+    public void append (E x){
+        newNode = new Node<E> (x);
+        if (head==null) {
+            // for very first mode
+            head = new Node(x);
+        } else {
+            current = head;
+            // track down to tail node
+            while (current.next != null)
+                current = current.next; // add in the new node
+            current.next = newNode;
+        }
+
+
+    }
+    public String toString(){
+        String s = "";
+        current = head;
+        while (current!=null) {
+            s += current.data.toString();
+            current = current.next;
+        }
+        return (s);
+    }
+}
+class Node<E> {
+    E data;
+    Node<E> next = null;
+    Node<E> previous = null;
+    Node (E data) {
+        this.data = data;
+    }
 }
